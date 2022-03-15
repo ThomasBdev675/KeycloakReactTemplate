ef5f7a842fa3c735c27c1dd1b443fd8aa0e71616
Thomas Bauer <thomasb_dev@outlook.com>
2022-03-15 14:59:29 +0100

    update: Keaycloac config

--- src/services/KeycloakService.js
+++ src/services/KeycloakService.js
@@ -1,23 +1,23 @@
 import Keycloak from "keycloak-js";
 
-const _keycloak = new Keycloak("keycloak.json");
+const _keycloak = new Keycloak("../keycloak.json");
 
 /**
  * Initialise Keycloak
- * @param {function} onAuthenticated 
+ * @param {function} onAuthenticated
  */
-const initKeycloak = onAuthenticated => {
-	_keycloak
-		.init({
-			onLoad: "check-sso",
-			silentCheckSsoRedirectUri:
-				window.location.origin + "/silent-check-sso.html",
-			pkceMethod: "S256",
-		})
-		.then(onAuthenticated)
-		.catch(() =>{
-			console.log('here');
-		})
+const initKeycloak = (onAuthenticated) => {
+  _keycloak
+    .init({
+      onLoad: "check-sso",
+      silentCheckSsoRedirectUri:
+        window.location.origin + "/silent-check-sso.html",
+      pkceMethod: "S256",
+    })
+    .then(onAuthenticated)
+    .catch(() => {
+      console.log("here");
+    });
 };
 
 /**
@@ -44,13 +44,11 @@ const isLoggedIn = () => !!_keycloak.token;
 
 /**
  * Update the token
- * @param {function} successCallback 
+ * @param {function} successCallback
  * @returns void
  */
 const updateToken = (successCallback) =>
-	_keycloak.updateToken(5)
-		.then(successCallback)
-		.catch(doLogin);
+  _keycloak.updateToken(5).then(successCallback).catch(doLogin);
 
 /**
  * Get the current users' username
@@ -60,23 +58,23 @@ const getUsername = () => _keycloak.tokenParsed?.preferred_username;
 
 /**
  * Check if user has any of the given roles
- * @param {Array<string>} roles 
+ * @param {Array<string>} roles
  * @returns boolean
  */
 const hasRole = (roles) => roles.some((role) => _keycloak.hasRealmRole(role));
 
 const KeycloakService = {
-	initKeycloak,
-	doLogin,
-	doLogout,
-	getToken,
-	isLoggedIn,
-	updateToken,
-	getUsername,
-	hasRole,
+  initKeycloak,
+  doLogin,
+  doLogout,
+  getToken,
+  isLoggedIn,
+  updateToken,
+  getUsername,
+  hasRole,
 };
 
-export default KeycloakService
+export default KeycloakService;
 
 /**
  * Special thanks to Niko Köbler for his tutorial on React with keycloak
